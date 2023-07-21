7b675e90924e76e9ed4122f7fc626a6181355006
wmzspace <sc21m2w@leeds.ac.uk>
2023-07-21 19:08:16 +0800

    配置 Element Plus & eslint

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import ElementPlus from "element-plus";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(ElementPlus);
+
+app.use(store).use(router).mount("#app");
